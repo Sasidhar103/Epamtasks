17610c045ac07c31e1fdd6e2422905f69cd6ab5d
Sasidhar103 <chitakanasasidhar@gmail.com>
2019-03-20 21:17:34 +0530

    first commit

---
+++ Epam/Epamtask1.java
@@ -0,0 +1,15 @@
+package Epam;
+
+/**
+ * Hello world!
+ *
+ */
+public class Epamtask1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
